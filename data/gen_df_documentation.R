f77269b11689d994c98449af50c4d41ea120ede9
Joey Stanley <joey_stanley@byu.edu>
2020-10-18 10:40:14 -0600

    changed "baby boom" to "boomer" and removed unnecessary column

--- data/gen_df_documentation.R
+++ data/gen_df_documentation.R
@@ -11,7 +11,7 @@ if(FALSE){
                                     "Awakening", "Liberty", "Republican", "Compromise",
                                     "Transcendental", "Gilded", "Progressive",
                                     "Missionary", "Lost", "G.I.", "Silent",
-                                    "Baby Boom", "Gen X", "Millennial", "Gen Z"),
+                                    "Boomer", "Gen X", "Millennial", "Gen Z"),
                           start = c(1435,1459,
                                     1497,1517,1542,1569,
                                     1594,1621,1649,1675,
@@ -20,9 +20,9 @@ if(FALSE){
                                     1865,1886,1908,1929,
                                     1946,1964,1984,2008)) %>%
     dplyr::mutate(end = c(dplyr::lead(start, default = 2031) - 1)) %>%
-    dplyr::arrange(-start) %>%
-    dplyr::rowwise() %>%
-    dplyr::mutate(range = list(c(start:end))) %>%
-    dplyr::ungroup()
-  use_data(gen_df)
+    dplyr::arrange(-start)
+    # dplyr::rowwise() %>%
+    # dplyr::mutate(range = list(c(start:end))) %>%
+    # dplyr::ungroup()
+  use_data(gen_df, overwrite = TRUE)
 }
